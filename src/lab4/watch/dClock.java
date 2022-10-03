01822d7f769195586c464e73fcd6f0544b3c2077
Energeenot <abramov.pandos@yandedx.ru>
2022-10-03 15:38:11 +0300

    небольшие изменения

--- src/lab4/watch/dClock.java
+++ src/lab4/watch/dClock.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public interface dClock {
     Random random = new Random();
-    int hours = random.nextInt(25);
+    int hours = random.nextInt(24);
     int minutes = random.nextInt(61);
     public void ptime();
 }
